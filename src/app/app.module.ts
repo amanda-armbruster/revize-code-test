fe093a6467ccf7355ddedfec0e7b5e2bd59f4243
Amanda Armbruster <Amandalynnarmbruster@gmail.com>
2020-09-16 20:29:50 -0400

    code test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DogFormComponent } from './dog-form/dog-form.component';
+import { FreedomFormComponent } from './freedom-form/freedom-form.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, DogFormComponent, FreedomFormComponent],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/freedom-form/freedom-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-freedom-form',
+  templateUrl: './freedom-form.component.html',
+  styleUrls: ['./freedom-form.component.css']
+})
+export class FreedomFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
